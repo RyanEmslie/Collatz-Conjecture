ba10e9815afc678bb6b92a901cdbf4ac2c3b0b48
RyanEmslie <ryan.emslie@gmail.com>
2019-01-12 12:50:09 -0500

    project complete - added input validation

--- app.js
+++ app.js
@@ -1,45 +1,79 @@
-const steps = [];
-
-const equation = function(start) {
-    const oddFunc = odd => 3 * odd + 1;
-    const evenFunc = even => even / 2;
-
-    const alg = startNum =>
-        startNum % 2 == 0 ? evenFunc(startNum) : oddFunc(startNum);
-
-    let result = alg(start);
-    // console.log(result);
-
-    if (result == 1) {
-        steps.push(result);
-    } else {
-        steps.push(result);
-        equation(result);
-    }
-};
+let steps = [];
+let count = 0;
 
 class UI {
+    static equation(start) {
+        const oddFunc = odd => 3 * odd + 1;
+        const evenFunc = even => even / 2;
+
+        const alg = startNum =>
+            startNum % 2 == 0 ? evenFunc(startNum) : oddFunc(startNum);
+
+        let result = alg(start);
+
+        if (result == 1) {
+            steps.push(result);
+        } else {
+            steps.push(result);
+            UI.equation(result);
+        }
+    }
+
     static displayNumbers(step) {
-        // console.log(`steps`, steps);
+        count += 1;
+        document.querySelector(
+            "#displayTotalSteps"
+        ).innerHTML = `Total number of steps: ${steps.length}`;
         const list = document.querySelector("#number-list");
         const row = document.createElement("tr");
-        row.innerHTML = `<td>${step.index}</td><td>${step}</td>`;
+        row.innerHTML = `<td>${count}</td><td>${step}</td>`;
         list.appendChild(row);
     }
 
-    // static clearFields() {
-    //     document.querySelector("#startNumber").value = 0;
-    // }
+    // resets array to zero on new submit
+    static resetStepArray() {
+        steps.length = 0;
+        count = 0;
+    }
+
+    // while tb has children, remove the first one
+    static deleteRows() {
+        let tb = document.querySelector("tbody");
+        while (tb.childNodes.length) {
+            tb.removeChild(tb.childNodes[0]);
+        }
+    }
+
+    // retrieves value from form
+    // validates for only positive integers and sends to equation
+    static submittedNumber() {
+        let enteredNum = document.querySelector("#startNumber").value;
+        enteredNum = parseInt(enteredNum);
+        console.log(enteredNum);
+        console.log(typeof enteredNum);
+        if (isNaN(enteredNum)) {
+            document.querySelector(
+                "#displayTotalSteps"
+            ).innerHTML = `Please enter a postive integer`;
+        } else if (enteredNum < 1) {
+            document.querySelector(
+                "#displayTotalSteps"
+            ).innerHTML = `Please enter a postive integer`;
+        } else {
+            UI.equation(enteredNum);
+        }
+        document.querySelector("#startNumber").value = " ";
+    }
 }
 
-//
-//! Event - Submit number
-//
 document.querySelector("#displayForm").addEventListener("submit", e => {
     e.preventDefault();
-    let enteredNum = document.querySelector("#startNumber").value;
-    enteredNum = parseInt(enteredNum);
-    equation(enteredNum);
-    console.log(`steps`, steps);
+
+    UI.resetStepArray();
+
+    UI.deleteRows();
+
+    UI.submittedNumber();
+
     steps.forEach(step => UI.displayNumbers(step));
 });
